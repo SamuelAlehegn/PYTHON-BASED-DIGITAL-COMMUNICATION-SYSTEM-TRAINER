49505f329b12a2731bf6841d616546b084938bc9
SamuelAlehegn <hadis.samuelalehegn@gmail.com>
2023-01-01 00:18:33 +0300

    sidebar modified

--- digitalCommunication/digitalCommunication/settings.py
+++ digitalCommunication/digitalCommunication/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS=[BASE_DIR, 'AppDigitalCommunication/templates']
+STATICFILES_DIRS=[BASE_DIR, 'digitalCommunication/static']
 # STATIC_ROOT=[BASE_DIR, 'static']
 
 # Default primary key field type
